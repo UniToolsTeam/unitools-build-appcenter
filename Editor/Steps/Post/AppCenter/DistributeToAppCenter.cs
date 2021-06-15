04c3fb0a8817e60f8e3da0f3c157eaa865757b78
Stanislav Felinskyi <stanisalv.felinskyi@innovecs.com>
2021-06-15 11:12:12 +0300

    Misc: Initial

---
+++ Editor/Steps/Post/AppCenter/DistributeToAppCenter.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using UniTools.CLI;
+using UniTools.IO;
+using UnityEngine;
+
+namespace UniTools.Build.AppCenter
+{
+    [CreateAssetMenu(
+        fileName = nameof(DistributeToAppCenter),
+        menuName = nameof(UniTools) + "/Build/Steps/" + nameof(AppCenter) + "/Post/" + nameof(DistributeToAppCenter)
+    )]
+    public sealed class DistributeToAppCenter : ScriptablePostBuildStep
+    {
+        [SerializeField] private string m_appName = default;
+        [SerializeField] private string m_group = "Collaborators";
+        [SerializeField] private string m_apiToken = default;
+        [SerializeField] private PathProperty m_builtFilePath = default;
+
+        public override async Task Execute(string pathToBuiltProject)
+        {
+            await Task.CompletedTask;
+            string command = $"distribute release " +
+                $" --app {m_appName}" +
+                $" --file {m_builtFilePath.ToString()}" +
+                $" --group \"{m_group}\"" +
+                $" --token {m_apiToken}";
+
+            ToolResult result = Cli.Tool<CLI.AppCenter>().Execute(command, ProjectPath.Value);
+
+            if (result.ExitCode != 0)
+            {
+                throw new PostBuildStepFailedException($"{nameof(DistributeToAppCenter)}: Failed! {result.ToString()}");
+            }
+        }
+    }
+}
